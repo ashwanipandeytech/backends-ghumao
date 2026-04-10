159798fbf0a2b0ccd6101f54fafc2c82f4ba5054
rohit_myqsoft <rohit.singh@myqsoft.com>
2026-04-10 22:04:04 +0530

    some changes

--- app/Http/Controllers/API/EnquieryController.php
+++ app/Http/Controllers/API/EnquieryController.php
@@ -37,7 +37,7 @@ class EnquieryController extends BaseController
      * @return \Illuminate\Http\Response
      */
 
-    public function add1111(Request $request)
+    public function add111(Request $request)
     {
         $input = $request->all();
         $validator = Validator::make($input, [
